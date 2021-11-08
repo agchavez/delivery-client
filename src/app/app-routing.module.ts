800752de4c585ad4445918f0cedfb382e8065d12
Daniela-2017 <danny15zavalalic@gmail.com>
2021-11-08 16:34:40 -0600

    NavBar clients

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -25,6 +25,10 @@ const routes: Routes = [
     loadChildren: ()=> import('./auth/auth.module').then(m => m.AuthModule)
   },
   {
+    path:'clients',
+    loadChildren: ()=> import('./clients/clients/clients.module').then(m => m.ClientsModule)
+  },
+  {
     path:'**',
     redirectTo: '404'
   },
