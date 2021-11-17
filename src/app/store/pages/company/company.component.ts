7758335a99fc028cd9190208c0bde0be43a6abd6
Daniela-2017 <danny15zavalalic@gmail.com>
2021-11-17 02:02:39 -0600

    3 modales

--- src/app/store/pages/company/company.component.ts
+++ src/app/store/pages/company/company.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-company',
@@ -7,10 +8,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CompanyComponent implements OnInit {
   panelOpenState:boolean = true;
-  constructor() { }
-
+  constructor(private modalService: NgbModal) { }
+  
   ngOnInit(): void {
   }
 
-
+  DetailProduct(modal:any){
+    this.modalService.open(modal, 
+      {size:'md',
+    })
+  }
 }
