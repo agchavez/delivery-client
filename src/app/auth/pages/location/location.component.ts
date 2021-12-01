809745cd79a074cd038f932dbe2653659ca7dff1
Daniela-2017 <danny15zavalalic@gmail.com>
2021-11-30 21:06:25 -0600

    location

--- src/app/auth/pages/location/location.component.ts
+++ src/app/auth/pages/location/location.component.ts
@@ -1,6 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { Validators, FormBuilder, FormGroup } from '@angular/forms';
 import * as mapboxgl  from 'mapbox-gl';
 import { environment } from '../../../../environments/environment';
+import { UbicationService } from '../../services/ubication.service';
 
 @Component({
   selector: 'app-location',
@@ -8,8 +10,23 @@ import { environment } from '../../../../environments/environment';
   styleUrls: ['./location.component.css']
 })
 export class LocationComponent implements OnInit {
+  ubications:any=[]
+  municipallies:any
+  munic:any
+    // Access ng-select
+   // @ViewChild(food) ngSelectComponent: NgSelectComponent;
 
-  constructor() { }
+    // Call to clear
+   // this.ngSelectComponent.handleClearClick();
+  
+  myForm:FormGroup = this.fb.group({
+    depto: ['',[Validators.required]],
+    muni: ['',[Validators.required]],
+    lat: ['',[Validators.required]],
+    long:['',[Validators.required]]
+  })
+
+  constructor(private ubicationService: UbicationService, private fb: FormBuilder) { }
 
   ngOnInit(): void {
     (mapboxgl as any).accessToken = environment.mapsToken;
@@ -17,6 +34,35 @@ export class LocationComponent implements OnInit {
     container: 'mapa',
     style: 'mapbox://styles/mapbox/streets-v11'
     });
+    this.getUbications()
+
   }
 
+  getUbications(){
+    this.ubicationService.getUbication().subscribe(
+      res=>{
+        this.ubications=res.ubications[0]
+
+        console.log('ubications:',this.ubications)
+      },
+      error=>{
+        console.log(error)
+      }
+    )
+    
+  }
+
+  getMunicipally(){
+    
+    this.munic=['']
+    this.munic=this.municipallies[0]
+    console.log(this.munic)
+    //this.municipallies=mun
+    //console.log(mun)
+    //this.municipal()
+  }
+
+  municipal(){
+    this.municipallies=this.municipallies[0]  
+  }
 }
