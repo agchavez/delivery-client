a6d244521f8b840ff1ec2727300f68907132d84f
agchavez <angel.chavez.325.ac@gmail.com>
2022-12-16 21:49:30 -0600

    feat: :sparkles: Configraciones locales

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,12 +4,12 @@
 
 export const environment = {
   production: false,
-  baseUrl: 'https://pidelow-backend-8zkq7.ondigitalocean.app/api',
+  baseUrl: 'http://localhost:8080/api',
   mapsToken: 'pk.eyJ1IjoiYWdjaGF2ZXoiLCJhIjoiY2t2cmlhamx0N3J5bjJ3bXNzbGM5ZHVxYiJ9.uBM5uzvD5g-p0_VT49IG0A'
 };
 
 /*
- * For easier debugging in development mode, you can import the following file
+ * For easier debugging   in development mode, you can import the following file
  * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
  *
  * This import should be commented out in production mode because it will have a negative impact
